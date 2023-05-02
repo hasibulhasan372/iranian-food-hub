a313eb4994561e46541dddb072bfd259302527b1
HasibulHasan <hasibulhasan372@gmail.com>
2023-05-02 07:01:27 +0600

    fetch the view recipepage by view recipe btn and also fetch the chef  api of those recipe

--- src/components/card/Recipe/Recipe.jsx
+++ src/components/card/Recipe/Recipe.jsx
@@ -4,18 +4,21 @@ import { useLoaderData } from 'react-router-dom';
 const Recipe = () => {
     const recipes = useLoaderData();
     const {chef_id} = recipes[0]
-    const [chefs, setChefs] = useState([])
+    const [chef, setChef] = useState([])
 
     useEffect(() => {
         fetch('http://localhost:5000/chef')
         .then(res => res.json())
-        .then(data => setChefs(data))
+        .then(data => {
+           const selectedChef = data.find(cook => cook.id === chef_id)
+           setChef(selectedChef)
+        })
     },[]);
-    
-    console.log(recipes, chefs, chef_id)
+   
+    console.log(chef.id)
     return (
         <div>
-            <h1>Hello Here is the recipe</h1>
+            <h1>Hello Here is the recipe: {chef.name} </h1>
         </div>
     );
 };
